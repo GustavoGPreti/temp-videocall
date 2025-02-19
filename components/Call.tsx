8fb1684c2bd11386f4bdf300e44dd4f5da750923
GustavoGPreti <gustavogpreti@gmail.com>
2025-02-19 17:44:27 -0300

    fix bugs

--- components/Call.tsx
+++ components/Call.tsx
@@ -1,4 +1,5 @@
 "use client";
+import next from "next";
 
 import AgoraRTC, {
   AgoraRTCProvider,
@@ -12,6 +13,7 @@ import AgoraRTC, {
   useRemoteAudioTracks,
   useRemoteUsers,
 } from "agora-rtc-react";
+import Link from "next/link";
 
 function Call(props: { appId: string; channelName: string }) {
   const client = useRTCClient(
@@ -22,12 +24,12 @@ function Call(props: { appId: string; channelName: string }) {
     <AgoraRTCProvider client={client}>
       <Videos channelName={props.channelName} AppID={props.appId} />
       <div className="fixed z-10 bottom-0 left-0 right-0 flex justify-center pb-4">
-        <a
+        <Link
           className="px-5 py-3 text-base font-medium text-center text-white bg-red-400 rounded-lg hover:bg-red-500 focus:ring-4 focus:ring-blue-300 dark:focus:ring-blue-900 w-40"
           href="/"
         >
           End Call
-        </a>
+        </Link>
       </div>
     </AgoraRTCProvider>
   );
