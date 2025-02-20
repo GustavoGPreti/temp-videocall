218f6cf619ee2785006212a2cc0b3a2277568029
GustavoGPreti <gustavogpreti@gmail.com>
2025-02-19 21:03:03 -0300

    dead

--- app/channel/[channelName]/page.tsx
+++ app/channel/[channelName]/page.tsx
@@ -1,12 +1,21 @@
 import Call from "@/components/Call";
+import { Metadata } from "next";
 
-export default function Page({ params }: { params: { channelName: string } }) {
-    return (
-        <main className="flex w-full flex-col">
-            <p className="absolute z-10 mt-2 ml-12 text-2xl font-bold text-gray-900">
-                {params.channelName!}
-            </p>
-            <Call appId={process.env.PUBLIC_AGORA_APP_ID!} channelName={params.channelName}></Call>
-        </main>
-    )
+interface PageProps {
+  params: { channelName: string };
+}
+
+export const metadata: Metadata = {
+  title: "Agora Call",
+};
+
+export default function Page({ params }: PageProps) {
+  return (
+    <main className="flex w-full flex-col">
+      <p className="absolute z-10 mt-2 ml-12 text-2xl font-bold text-gray-900">
+        {params.channelName}
+      </p>
+      <Call appId={process.env.NEXT_PUBLIC_AGORA_APP_ID!} channelName={params.channelName} />
+    </main>
+  );
 }
